6b5ab778f865f876d4820c12670e6aab05940a60
keyuwan9 <138041129+keyuwan9@users.noreply.github.com>
2025-12-01 19:40:28 +0000

    Deploying to gh-pages from @ keyuwan9/keyuwan9.github.io@867497a0433d218cf28e8731608d30cc9742606d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,19 +9,12 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-publications",
-          title: "publications",
+  },{id: "nav-academic-publications",
+          title: "academic publications",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
-          },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
           },
         },{id: "nav-teaching",
           title: "teaching",
